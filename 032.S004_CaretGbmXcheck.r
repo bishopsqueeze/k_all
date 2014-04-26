93907914b730e1c0c37dfcb6458e786e16ce84da
bishopsqueeze <alexstephens@db.local>
2014-04-26 08:53:02 -0400

    Enabled hold-out tests

--- 032.S004_CaretGbmXcheck.r
+++ 032.S004_CaretGbmXcheck.r
@@ -9,6 +9,8 @@
 library(gbm)
 library(plyr)
 library(caret)
+library(foreach)
+library(doMC)
 
 ##------------------------------------------------------------------
 ## Clear the workspace
@@ -35,6 +37,12 @@ setwd("/Users/alexstephens/Development/kaggle/allstate/data/gbm_scored_S004")
 ##------------------------------------------------------------------
 xcheck.list <- list()
 xcheck.out  <- list()
+
+##------------------------------------------------------------------
+## Set-up a sink
+##------------------------------------------------------------------
+writeLines(c(""), "construct_panel_logfile.txt")
+sink("construct_panel_logfile.txt", append=TRUE)
 
 ##******************************************************************
 ## Step 1: Load fit data/results for each panel and each choice prediction
@@ -79,6 +87,9 @@ for (i in 2:11) {
     xcheck.list[[tmp.sp]] <- tmp.raw
 }
 
+## !!! Note:  If you re-run the fit using a hold-out sample, make sure the saved
+##            output contains the raw data ... so you can easily match the fit
+##            to a customer_ID (if needed)
 
 ##******************************************************************
 ## Step 2: Expand all possible permutations of choices
@@ -91,7 +102,7 @@ ch.combos <- ch.combos[which(apply(ch.combos, 1, sum) >= 1), ]
 ## Step 3:  Loop over all shopping_pts and choice combos and create
 ## a predicted plan ... append to the original data so we can do comparisons
 ##******************************************************************
-for (i in 11:2) {
+for (i in 2:11) {
     
     cat("Iteration ",i, " ... of 11 \n")
     
@@ -99,11 +110,19 @@ for (i in 11:2) {
     tmp.sp      <- paste("SP_",ifelse(i < 10, paste("0",i,sep=""),i),sep="")
     tmp.xcheck  <- xcheck.list[[tmp.sp]]
 
+    ## preallocate a matrix
+    perm.mat    <- matrix("character", nrow=dim(tmp.xcheck)[1], ncol=dim(ch.combos)[1])
+    perm.col    <- vector("character", length=dim(ch.combos)[1])
+    
     ## loop over all possible combinations
     for (j in 1:nrow(ch.combos)) {
-        
+    
+        cat("Iteration j == ",j, "\n")
+
         ## extract the combo, and replicate to the size of the panel
-        tmp.idx  <- do.call(rbind, replicate(nrow(tmp.xcheck), ch.combos[j, ], simplify=FALSE))
+        m           <- ch.combos[j, ]
+        rownames(m) <- NULL
+        tmp.idx  <- m[rep(1, times=nrow(tmp.xcheck)),]
         
         ## isolate last-quoted and predicted results
         tmp.lq   <- tmp.xcheck[ , paste(LETTERS[1:7],"0",sep="")]
@@ -128,22 +147,87 @@ for (i in 11:2) {
         comb.id   <- paste(colnames(filt.out), collapse="")
         comb.pred <- apply(filt.out, 1, paste, collapse="")
         
-        tmp.xcheck[ , comb.id] <- comb.pred
+        #tmp.xcheck[ , comb.id] <- comb.pred
+        perm.mat[, j]  <- comb.pred
+        perm.col[j]    <- comb.id
     }
     
+    ## add columns
+    colnames(perm.mat) <- perm.col
+    
+    ## load the results
     tmp.xcheck[, paste(LETTERS[1:7],"T", sep="", collapse="")] <- apply(tmp.xcheck[, paste(LETTERS[1:7],"T",sep="")], 1, paste, collapse="")
     tmp.xcheck[, paste(LETTERS[1:7],"0", sep="", collapse="")] <- apply(tmp.xcheck[, paste(LETTERS[1:7],"0",sep="")], 1, paste, collapse="")
     
-    xcheck.out[[tmp.sp]] <- tmp.xcheck
+    ## save results into a list
+    xcheck.out[[tmp.sp]] <- cbind(tmp.xcheck, perm.mat)
 }
+
+
+##******************************************************************
+## Step 4: For each SP, compute the average agreement
+##******************************************************************
+
+## test on the final (and full) set of observations
+fin.res <- data.frame()
+for (i in 2:11) {
+
+    ## isolate the data
+    tmp.sp      <- paste("SP_",ifelse(i < 10, paste("0",i,sep=""),i),sep="")
+    tmp.out     <- xcheck.out[[tmp.sp]][, 25:ncol(xcheck.out[[tmp.sp]])]
+
+    tmp.res     <- vector("numeric", length=ncol(tmp.out))
+    for (j in 1:ncol(tmp.out)) {
+        tmp.res[j] <- mean(as.character(tmp.out[,1]) == as.character(tmp.out[,j]))
+    }
+    fin.res     <- rbind(fin.res, tmp.res)
+}
+colnames(fin.res) <- colnames(tmp.out)
+rownames(fin.res) <- names(xcheck.out)[order(names(xcheck.out))]
+fin.res           <- t(fin.res)
+fin.res           <- fin.res[2:nrow(fin.res),]
+
+
+
+## test using the last observed point for each
+sub.res <- matrix("character", nrow=sum(do.call(rbind, lapply(xcheck.out, dim))[,1]), ncol=ncol(xcheck.out[[1]]))
+for (i in 2:11) {
+    
+    ## isolate the data
+    tmp.sp      <- paste("SP_",ifelse(i < 10, paste("0",i,sep=""),i),sep="")
+    tmp.out     <- xcheck.out[[tmp.sp]][, 1:ncol(xcheck.out[[tmp.sp]])]
+    
+    if (i == 2) {
+        tmp.lo  <- 1
+        tmp.hi  <- nrow(tmp.out)
+    } else {
+        tmp.lo  <- tmp.hi + 1
+        tmp.hi  <- tmp.hi + nrow(tmp.out)
+    }
+    
+    sub.res[tmp.lo:tmp.hi, ]    <- sapply(tmp.out, function(x) { (as.character(x)) } )
+}
+sub.res             <- data.frame(sub.res)
+colnames(sub.res)   <- colnames(xcheck.out[[1]])
+
+#sub.res             <- sub.res[ order(sub.res$customer_ID, sub.res$shopping_pt), ]
+#rownames(sub.res)   <- 1:nrow(sub.res)
+
+#a <- tapply(sub.res$shopping_pt, sub.res$customer_ID, function(x){ rownames(x)[which(x == max(x))]})
+
+
+
+### !!!!! YOU HAVE WORK TO DO CLEANING THIS MESS UP ###
 
 
 ##------------------------------------------------------------------
 ## Save results
 ##------------------------------------------------------------------
-save(xcheck.list, ch.combos, xcheck.out, file="/Users/alexstephens/Development/kaggle/allstate/data/S004_CaretGbmXcheck.Rdata")
+save(xcheck.list, ch.combos, xcheck.out, fin.res, sub.res,
+            file="/Users/alexstephens/Development/kaggle/allstate/data/S004_CaretGbmXcheck_v3.Rdata")
 
 
-
-
-
+##------------------------------------------------------------------
+## Close sink
+##------------------------------------------------------------------
+sink()
